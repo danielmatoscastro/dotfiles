ebbc45babff8f69d4b36227b00779b10bcec5bb3
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-07-21 07:57:41 -0300

    small refactors to turn code ore general.

--- rclone/rclone.sh
+++ rclone/rclone.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-folders=(Documentos faculdade Imagens)
+folders=("Documentos" "faculdade" "Imagens")
 
-echo "rclone run at $(date)" >> /home/daniel/.rclone.log
+echo "rclone run at $(date)" >> "$HOME/.rclone.log"
 for folder in ${folders[@]}; do    
-    rclone sync --verbose /home/daniel/"$folder" google-drive:rclone-desktop/"$folder"
+    rclone sync --verbose "$HOME/$folder" "google-drive:rclone-desktop/$folder"
 done
 
