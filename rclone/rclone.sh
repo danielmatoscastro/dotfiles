2ed6b0681b6311dde30ffe72089948dc452189df
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-07-21 07:48:33 -0300

    add log

--- rclone/rclone.sh
+++ rclone/rclone.sh
@@ -2,6 +2,7 @@
 
 folders=(Documentos faculdade Imagens)
 
+echo "rclone run at $(date)" >> /home/daniel/.rclone.log
 for folder in ${folders[@]}; do    
     rclone sync --verbose /home/daniel/"$folder" google-drive:rclone-desktop/"$folder"
 done
