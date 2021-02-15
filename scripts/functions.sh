212de3a1acac270f8d6382179538e66363fcc9b3
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-02-15 13:57:17 -0300

    add function to convert md files to pdf format.

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -3,3 +3,10 @@
 compress_video_with_h265() {
     ffmpeg -i $1 -vcodec libx265 -crf 28 $2
 }
+
+md2pdf(){
+    pdf=${@[-1]}
+    md=${@:1:${#}-1}
+
+    pandoc -V geometry:margin=1in -s -o $pdf "$md"
+}
